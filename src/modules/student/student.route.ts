d5a317bee9d1312278d0fa9124b86df49187ea55
Mainu0177 <mainuddinhassan658@gmail.com>
2026-04-03 12:53:18 +0600

    Feature(Student): Student route and schema

---
+++ src/modules/student/student.route.ts
@@ -0,0 +1,16 @@
+
+import { Router } from "express";
+
+import { studentController } from "./student.controller";
+import { authMiddleware, roleMiddleware } from "../../middlewares/auth";
+
+const router:Router = Router();
+
+//* Only students can access these routes
+router.get("/profile",authMiddleware,roleMiddleware(["STUDENT"]), studentController.getProfile);
+router.put("/profile",authMiddleware,roleMiddleware(["STUDENT"]), studentController.updateProfile);
+router.post("/profile/avater-change",authMiddleware,roleMiddleware(["STUDENT"]), studentController.updateProfile);
+router.get("/:id/dashboard/stats",authMiddleware,roleMiddleware(["STUDENT"]),studentController.getStudentDashboardStats);
+
+
+export default router;
